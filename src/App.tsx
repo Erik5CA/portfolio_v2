1b069dee51c56a0d80db20afa4c34b173bc53712
Erik5CA <erik.castillo.anzures@gmail.com>
2024-10-11 18:36:34 -0600

    main files

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+import Projects from "./sections/Projects";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <ToastContainer
+        position="bottom-right"
+        autoClose={4000}
+        theme="dark"
+        hideProgressBar
+      />
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Experience />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
